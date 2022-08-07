e71ec3b97a24a062e740aa63ceabdbceafcd45c3
Samantha Dorrell <samantha.dorrell@yahoo.co.uk>
2022-08-07 08:15:37 +0100

    Update index.jsx

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars } from "react-icons/fa";
-import { AiOutlineClose, AiOutlineBars } from "react-icons/ai"
-import { SidebarData } from './data'
-import './index.css' 
+import { AiOutlineClose } from "react-icons/ai";
+import { SidebarData } from './data';
+import './index.css' ;
 
 
 
@@ -18,6 +18,7 @@ function Navbar() {
             <Link to='#' className='menu-bars'>
               <FaBars onClick={showSidebar} />
             </Link>
+            <h1 id="navbrand"><a className="nav-brand" href="/">Artemis Camping</a></h1>
           </div>
           <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
             <ul className='nav-menu-items' onClick={showSidebar}>
@@ -29,7 +30,7 @@ function Navbar() {
               {SidebarData.map((item, index) => {
                 return (
                   <li key={index} className={item.cName}>
-                    <Link to={item.path}>
+                    <Link className="nav-item-link" to={item.path}>
                       {item.icon}
                       <span>{item.title}</span>
                     </Link>
