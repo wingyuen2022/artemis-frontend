62ca4f3e05fd39bf153ad9a12c4dc5c22265f0ed
Steven Olds <nathanolds02@gmail.com>
2022-08-08 12:28:21 +0100

    Adding Calendar Component

--- src/pages/Duration/index.js
+++ src/pages/Duration/index.js
@@ -1,46 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setTitle } from "../../actions";
-import Button from 'react-bootstrap/Button';
-import DurationForm from '../../components/DurationForm';
+import Button from "react-bootstrap/Button";
+import CalendarComponent from "../../components/CalendarComponent";
 
 const Duration = () => {
-    //const { xxx } = useParams();
-    //const [ curXXX, setCurXXX] = useState(null);
+  //const { xxx } = useParams();
+  //const [ curXXX, setCurXXX] = useState(null);
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    dispatch(setTitle("Duration"));
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  dispatch(setTitle("Duration"));
 
-    const handleBack = () => {
-		navigate('-1');
-	};
+  const handleBack = () => {
+    navigate("-1");
+  };
 
-    const handleEdit = () => {
-		navigate('/edit/duration');
-	};
+  const handleEdit = () => {
+    navigate("/edit/duration");
+  };
 
-    useEffect(()=>{
-        
-    }, []);
+  useEffect(() => {}, []);
 
-    const renderHTML = () => {
-        return (
-            <>
-                <div className="row">
-                    <div className="col">
-                        <DurationForm />
-                    </div>
-                    <div className="col">
-                        <Button onClick={handleBack}>Back</Button>
-                        <Button onClick={handleEdit}>Edit</Button>
-                    </div>
-                </div>
-            </>
-        )
-    };
+  const renderHTML = () => {
+    return (
+      <>
+        <div className="row">
+          <div className="col">
+            <CalendarComponent />
+          </div>
+          <div className="col">
+            <Button onClick={handleBack}>Back</Button>
+            <Button onClick={handleEdit}>Edit</Button>
+          </div>
+        </div>
+      </>
+    );
+  };
 
-    return renderHTML();
+  return renderHTML();
 };
 export default Duration;
