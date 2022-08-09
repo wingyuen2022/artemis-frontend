223ebe6bbe069a304ea924d3174a3d563dfd435f
Steven Olds <nathanolds02@gmail.com>
2022-08-09 11:11:55 +0100

    adding map and google directions components

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,46 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setTitle } from "../../actions";
-import Button from 'react-bootstrap/Button';
-import MapComponent from '../../components/MapComponent';
+import Button from "react-bootstrap/Button";
+import MapComponent from "../../components/MapComponent";
 
 const Map = () => {
-    //const { xxx } = useParams();
-    //const [ curXXX, setCurXXX] = useState(null);
+  //const { xxx } = useParams();
+  //const [ curXXX, setCurXXX] = useState(null);
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    dispatch(setTitle("Map"));
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  dispatch(setTitle("Map"));
 
-    const handleBack = () => {
-		navigate('-1');
-	};
+  const handleBack = () => {
+    navigate("-1");
+  };
 
-    const handleEdit = () => {
-        navigate('/edit/map');
-	};
+  const handleEdit = () => {
+    navigate("/edit/map");
+  };
 
-    useEffect(()=>{
-        
-    }, []);
+  useEffect(() => {}, []);
 
-    const renderHTML = () => {
-        return (
-            <>
-                <div className="row">
-                    <div className="col">
-                        <MapComponent />
-                    </div>
-                    <div className="col">
-                        <Button onClick={handleBack}>Back</Button>
-                        <Button onClick={handleEdit}>Edit</Button>
-                    </div>
-                </div>
-            </>
-        )
-    };
+  const renderHTML = () => {
+    return (
+      <>
+        <div className="row">
+          <div className="col">
+            <MapComponent />
+          </div>
+          <div className="col">
+            <Button onClick={handleBack}>Back</Button>
+            <Button onClick={handleEdit}>Edit</Button>
+          </div>
+        </div>
+      </>
+    );
+  };
 
-    return renderHTML();
+  return renderHTML();
 };
 export default Map;
