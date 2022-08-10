ccc38775fe2f9765a2c13b577a18d6eedc4c5f38
Samantha Dorrell <samantha.dorrell@yahoo.co.uk>
2022-08-10 11:44:09 +0100

    finished checklist and styles

--- src/pages/Checklist/index.js
+++ src/pages/Checklist/index.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch } from "react-redux";
 import { setTitle } from "../../actions";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { caravan } from "../../assets/images/camping";
 import "./Checklist.css";
 
-
-
-export default function Checklist() {
-  
+export default function Checklist(id) {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   dispatch(setTitle("Checklist"));
   
@@ -80,7 +79,6 @@ export default function Checklist() {
     <Container className="camping-checklist">
         <div className="checklist-heading">
             <img className="caravan-icon" src={caravan} alt="caravan"></img>
-
             <h4>Make sure you have everything you need with this handy checklist!</h4>
         </div>
         <div className="checklist-add-item">
@@ -111,10 +109,41 @@ export default function Checklist() {
 									<FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityIncrease(index)} />
 								</button>
 							</div>
+              <div className="assignee">
+                  <select id="assignee" name="assignee">
+                      <option value="Anyone">Anyone</option>
+                      <option value="Edgar">Edgar</option>
+                      <option value="Nathan">Nathan</option>
+                      <option value="Sam">Sam</option>
+                      <option value="Summira">Summira</option>
+                      <option value="Wing">Wing</option>
+                  </select>
+              </div>
 						</div>
 					))}
-          </div>
+          
           <div className='total'>Total: {totalItemCount}</div>
+            <div className="btn-container">
+                <div className="btn-row">
+                    <input id="id" type="number" name="id" hidden/><br />
+                    <Button id="back-btn" onClick={()=>{
+                        if (window.confirm("Confirm without saving?")) {
+                            navigate('/view/checklist');
+                        }
+                    }}>Back</Button>
+                    <Button id="delete-btn" variant="danger" onClick={()=>{
+                        if (window.confirm("Confirm to delete?")) {
+                            alert('deleted');
+                            navigate('/view/checklist');
+                        }
+                    }} hidden={id === null}>Delete</Button>
+                    <Button id="save-btn" onClick={()=>{
+                        alert('saved');
+                        navigate('/view/checklist');
+                    }}>Save</Button>
+                </div>
+              </div>
+            </div>
     </Container>
     </>
   );
