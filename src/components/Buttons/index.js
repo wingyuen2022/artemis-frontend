6a77bcb7fe29f84dabd9f70ad635cac262568a4c
Summira Hussain <summira508411@gmail.com>
2022-08-10 12:46:58 +0100

    work

--- src/components/Buttons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ChecklistAddItemBtn } from "./checklist-add-item";
-export { default as ChecklistRemoveItemBtn } from "./checklist-remove-item";
-export { default as ChecklistEditItemBtn } from "./checklist-edit-item";
-export { default as BackBtn } from "./back-btn";
